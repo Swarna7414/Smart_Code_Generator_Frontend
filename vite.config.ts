26b9b1af87e676250dabfaf481c63bd628319ad6
swarna7414 <swarnasaisankar333@gmail.com>
2026-03-22 16:47:59 -0500

    added pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/Frontend/',
+  base: '/Smart_Code_Generator_Frontend/',
   plugins: [
     react(),
     tailwindcss(),
